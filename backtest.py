bf416b6469ce33844132a9d498d14d25157de156
Ian Luo <ian@ianluo.com>
2018-01-06 02:49:07 -0500

    macd

--- backtest.py
+++ backtest.py
@@ -8,12 +8,13 @@ class Test:
     def __init__(self, strategy):
         self.short = 1
         self.long = 1
+        self.signal = 9
         self.results = []
         self.sample_result = {}
 
         self.strategy = strategy
         self.request_params = {}
-        self.best_params = {"short": -1, "long": -1, "profit": -1}
+        self.best_params = {"short": -1, "long": -1, "signal": -1, "profit": -1}
 
     def make_params(self):
         strat = strategy_params[self.strategy]
@@ -21,19 +22,22 @@ class Test:
         compiled_params["data"]["gekkoConfig"][self.strategy] = strat
         compiled_params["data"]["gekkoConfig"][self.strategy]["short"] = self.short
         compiled_params["data"]["gekkoConfig"][self.strategy]["long"] = self.long
+        compiled_params["data"]["gekkoConfig"][self.strategy]["signal"] = self.signal
         compiled_params["data"]["gekkoConfig"]["tradingAdvisor"]["method"] = self.strategy
         self.request_params = compiled_params
 
     def execute(self):
-        for s in range(1, 60, 2):
-            self.short = s
-            for l in range(1, 60, 2):
-                print("Current params: " + str(s) + " " + str(l))
-                self.long = l
+        for sig in range(1, 20, 2):
+            self.signal = sig
+            for s in range(1, 60, 2):
+                self.short = s
+                for l in range(1, 60, 2):
+                    print("Current params: " + str(s) + " " + str(l) + " " + str(sig))
+                    self.long = l
 
-                self.make_params()
-                response = self.send_request()
-                self.parse_response(response)
+                    self.make_params()
+                    response = self.send_request()
+                    self.parse_response(response)
         self.write_to_csv()
         print(self.best_params)
 
@@ -48,29 +52,29 @@ class Test:
         report = response.json()["report"]
         self.sample_result = report
 
-        list_to_write = [self.short, self.long]
+        list_to_write = [self.short, self.long, self.signal]
         list_to_write.extend(report.values())
         self.results.append(list_to_write)
 
         if report["profit"] > self.best_params["profit"]:
-            self.best_params = {"short": self.short, "long": self.long, "profit": report["profit"]}
+            self.best_params = {"short": self.short, "long": self.long, "signal": self.signal, "profit": report["profit"]}
             print(self.best_params)
 
     def write_to_csv(self):
         with open(self.strategy + ".csv", "w") as csv_file:
             writer = csv.writer(csv_file, 'excel')
-            header_row = ["short", "long"]
+            header_row = ["short", "long", "signal"]
             header_row.extend(self.sample_result.keys())
             writer.writerow(header_row)
             for line in self.results:
                 writer.writerow(line)
 
 def main():
-    test = Test("DEMA")
+    test = Test("MACD")
     test.execute()
 
 
-strategy_params = {"DEMA": {"short": 10, "long": 21, "thresholds": {"down": -0.025, "up": 0.025}}}
+strategy_params = {"MACD": {"short": 10, "long": 21, "signal": 9, "thresholds": {"down": -0.025, "up": 0.025}}}
 
 gekko_params = {
     "gekkoConfig": {"watch": {"exchange": "gdax",
@@ -92,7 +96,7 @@ gekko_params = {
                                        "candleSize": 60,
                                        "historySize": 10
                                        },
-                    "backtest": {"daterange": {"from": "2016-12-13T02:05:00Z",
+                    "backtest": {"daterange": {"from": "2017-12-12T02:05:00Z",
                                                "to": "2017-12-13T03:05:00Z"}
                                  },
                     "performanceAnalyzer": {"riskFreeReturn": 2,
